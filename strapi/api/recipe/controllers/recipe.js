430588ee8812e6b23522cc1aa18152805672124d
Vlad Savilo <vladislavsav95@gmail.com>
2021-05-07 01:22:01 +0300

    New points for recipe

--- strapi/api/recipe/controllers/recipe.js
+++ strapi/api/recipe/controllers/recipe.js
@@ -22,6 +22,101 @@ module.exports = {
     },
 
     /**
+     * Retrieve records of recipes.
+     *
+     * @return {Array}
+     */
+    async findCustom(ctx) {
+        let entities, count = 0
+
+        if (ctx.query._q) {
+            entities = await strapi.services.recipe.search(ctx.query);
+            count = await strapi.services.recipe.countSearch(ctx.query);
+        } else {
+            entities = await strapi.services.recipe.find(ctx.query);
+            count = await strapi.services.recipe.count(ctx.query);
+        }
+
+        let recipes = entities.map(entity => {
+            let recipe = sanitizeEntity(entity, { model: strapi.models.recipe })
+
+            return {
+                id: recipe.id,
+                title: recipe.title,
+                difficult: recipe.difficult,
+                time_to_prepare: recipe.time_to_prepare,
+                photography: recipe.photography 
+                    ? recipe.photography[0].formats && recipe.photography[0].formats.small
+                        ? recipe.photography[0].formats.small
+                        : recipe.photography[0].formats.thumbnail 
+                    : null
+            }
+        })
+
+        return {
+            count,
+            recipes
+        }
+    },
+
+    /**
+     * Get info for filters
+     */
+    async getFiltersInfo(ctx) {
+        let entitiesRecipes = await strapi.services.recipe.find();
+        let entitiesCategories = await strapi.services.categories.find();
+
+        let recipes = entitiesRecipes.map(entity => {
+            let recipe = sanitizeEntity(entity, { model: strapi.models.recipe })
+
+            return {
+                id: recipe.id,
+                time_to_prepare: recipe.time_to_prepare,
+                ingredients: recipe.ingredients
+            }
+        })
+
+        const arrayOfRecipesTime = recipes.map(recipe => recipe.time_to_prepare)
+        const timeToPrepare = {
+            min: Math.min(...arrayOfRecipesTime),
+            max: Math.max(...arrayOfRecipesTime),
+        }
+
+        let ingredientsList = []
+        if (recipes.length) {
+            let ingredients = []
+
+            recipes.forEach(recipe => {
+                let curCat = recipe.ingredients.map(ingredient => {
+                    return {
+                        key: ingredient.key.toLowerCase(),
+                    }
+                })
+                ingredients = ingredients.concat(curCat)
+            })
+            
+            ingredientsList = Array.from(new Set(ingredients.map(cat => cat.key.trim())))
+                .filter(item => item)
+                .sort((a, b) => (a > b) ? 1 : ((b > a) ? -1 : 0))
+        }
+
+        const categories = entitiesCategories.map(entity => {
+            let category = sanitizeEntity(entity, { model: strapi.models.categories })
+
+            return {
+                id: category.id,
+                title: category.title
+            }
+        }).sort((a, b) => (a.title.toLowerCase() > b.title.toLowerCase()) ? 1 : ((b.title.toLowerCase() > a.title.toLowerCase()) ? -1 : 0))
+
+        return {
+            categories: categories,
+            ingredients: ingredientsList,
+            time: timeToPrepare
+        }
+    },
+
+    /**
      * Update likes in recipe
      *
      * @return {Object}
